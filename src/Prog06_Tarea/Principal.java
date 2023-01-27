af4ce2bf896552be674737a3188c9f15ac980642
Ivan Molina <ivanmolinamas@gmail.com>
2023-01-27 11:42:22 +0100

    Primer commit

---
+++ src/Prog06_Tarea/Principal.java
@@ -0,0 +1,71 @@
+package Prog06_Tarea;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+/**
+ *
+ * @author IVAN Clase principal
+ */
+public class Principal {
+
+    static Scanner teclado = new Scanner(System.in);
+
+    public static int mostrarMenu() {
+        System.out.println("GESTION DE VEHICULOS DEL CONCESIONARIO");
+
+        System.out.println("1 - Nuevo vehiculo");
+        System.out.println("2 - Listar vehiculos");
+        System.out.println("3 - Buscar vehiculo");
+        System.out.println("4 - Modificar kilometros de vehiculo");
+        System.out.println("5 - Salir");
+
+        int opcion = teclado.nextInt();
+        teclado.nextLine();
+        return opcion;
+    }
+
+    public static void main(String[] args) {
+        int opcion;
+
+        String marca, matricula, descripcion, propietario, dni;
+        int kilometros, precio, nuevosKilometros;
+        LocalDate matri_fecha;
+        int matri_dia, matri_mes, matri_anyo;
+
+        boolean check = true;
+        
+        //vehiculo coche1 = null;
+        
+        System.out.println("BIENVENIDO");
+        
+        
+        do{
+          opcion = mostrarMenu();  
+          
+          switch(opcion){
+              case 1:
+                  System.out.println("Nuevo Vehiculo");
+              break;
+              case 2:
+                  System.out.println("Listar Vehiculos");
+              break;
+              case 3:
+                  System.out.println("Buscar Vehiculo");
+              break;
+              case 4:
+                  System.out.println("Modificar kilometros");
+              break;
+              case 5:
+                  System.out.println("Fin del programa. Adios!");
+              break;
+              default:
+                  System.out.println("Opcion incorrecta.");
+              
+          }
+        }while(opcion != 5);
+        
+        
+    }
+
+}
