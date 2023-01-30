de4948b64c59d734c02e6b784dbc74da88458e62
Ivan Molina <ivanmolinamas@gmail.com>
2023-01-30 17:37:32 +0100

    Codigo practiamente funcionando, falta repasar

--- src/Prog06_Tarea/Principal.java
+++ src/Prog06_Tarea/Principal.java
@@ -69,60 +69,66 @@ public class Principal {
                   //hay que buscar la forma de que se haga un objeto nuevo cada vez
                   // maximo 50 coches
                   System.out.println("Nuevo Vehiculo");
-                  
+
                   // inicio de pedir datos al usuario para crear un vehiculo nuevo
                   // los datos se guadaran en las variables, estas si son correctas 
                   // se pasaran todas a la vez al constructor.
-                  
                   System.out.println("Introduce la marca del vehiculo");
                   marca = teclado.nextLine();
-                  
+
+                  // se pide la descripcion del vehiculo y se añade a la variable descripcion
                   System.out.println("Introduce la descripcion del vehiculo");
                   descripcion = teclado.nextLine();
-                  
 
+                  // se pide matricula
                   // con do while creamos bucle hasta que la matricula sea correcta
-                  do{
+                  do {
                       System.out.println("Introduce la matricula del vehciulo en formato NNNNLLL");
-                      matricula = teclado.nextLine(); 
+                      matricula = teclado.nextLine();
                       // con el if lanzamos el mensaje de matricula incorrecta
-                      if(!Validar.validarMatricula(matricula)){
+                      if (!Validar.validarMatricula(matricula)) {
                           System.out.println("Matricula inocrrecta");
                       }
-                  }while(!Validar.validarMatricula(matricula));
-                  
+                  } while (!Validar.validarMatricula(matricula));
+
+                  // se piden kiometros
                   // igual que antes, con do while comprobamos que los kilometros son positivos
-                  do{
+                  do {
                       System.out.println("Introduzca los kilometros");
                       kilometros = teclado.nextInt();
-                      teclado.nextLine(); 
-                      if(0>kilometros){
+                      teclado.nextLine();
+                      if (0 > kilometros) {
                           System.out.println("Kilometros incorrectos");
                       }
-                  }while(0 > kilometros);
-                 
-                 
+                  } while (0 > kilometros);
+
+                  //se pide el precio
                   System.out.println("Introduce precio del vehiculo");
                   precio = teclado.nextInt();
                   teclado.nextLine();
-                  
+
+                  // se pide el nombre del propietario
+                  // se valida que tenga dos espacios, a traves de la calse validar
+                  // con el metodo validarNombre
                   do {
                       System.out.println("Introduce el nombre del propietario");
                       propietario = teclado.nextLine();
-                      if(!Validar.validarNombre(propietario)){
+                      if (!Validar.validarNombre(propietario)) {
                           System.out.println("Nombre incorrecto, introduzca nombre y apellidos");
                       }
                   } while (!Validar.validarNombre(propietario));
 
+                  //se pide el DNI
                   // Validación del DNI, con un do while se pedira hasta que sea correcto
-                  do{
-                    System.out.println("Introduce el DNI del propietario");
-                  dni = teclado.nextLine(); 
-                  if(!Validar.validarDNI(dni)){
-                      System.out.println("DNI incorrecto.");
-                  }
-                  }while(!Validar.validarDNI(dni));
-  
+                  do {
+                      System.out.println("Introduce el DNI del propietario");
+                      dni = teclado.nextLine();
+                      if (!Validar.validarDNI(dni)) {
+                          System.out.println("DNI incorrecto.");
+                      }
+                  } while (!Validar.validarDNI(dni));
+
+                  // se pide la fecha de forma individual dia, mes y año
                   // validar fecha correcta
                   do {
                       correcto = true;
@@ -138,50 +144,45 @@ public class Principal {
 
                           System.out.println("Introduce el año de matricuiacon");
                           anyo_matri = teclado.nextInt();
-//                          teclado.nextLine();
-                          
+
                           matri_fecha = LocalDate.of(anyo_matri, mes_matri, dia_matri);
                       } catch (InputMismatchException e) {
                           correcto = false;
                           teclado.next();
                           System.out.println("Fecha incorrecta");
-                      }catch(DateTimeException e){
+                      } catch (DateTimeException e) {
                           correcto = false;
                           System.out.println("Fecha incorrecta");
                       }
-                      
-                      
-                      
-                      
-//                      if (!Validar.validaFecha(matri_fecha)) {
-//                          System.out.println("Fecha incorrecta, superior a la actual");
-//                          correcto = true;
-//                      }
-                  } while (!correcto); 
+                      // se puede mejorar validando que la fecha es inferior a la actual
+                      // si todo lo anterio resta correcto se sale del bucle
+                  } while (!correcto);
                   // COMPROBAR FECHA????????????????????????????????
-//                  boolean comprobarfecha=true;
-//                  do{
-//                      if(Validar.validaFecha(matri_fecha)){
-//                          comprobarfecha = true;
-//                      }else{
-//                          comprobarfecha=false;
-//                      }
-//                  }while(!comprobarfecha);
-                  
+                  //                  boolean comprobarfecha=true;
+                  //                  do{
+                  //                      if(Validar.validaFecha(matri_fecha)){
+                  //                          comprobarfecha = true;
+                  //                      }else{
+                  //                          comprobarfecha=false;
+                  //                      }
+                  //                  }while(!comprobarfecha);
+
                   // ahora introducimos los datos en un vehiculo y posteriormente al array
-                  
-                  v = new Vehiculo(marca,matricula,descripcion,propietario,dni,kilometros,precio,matri_fecha);
-                  
-//                  marca;
-//        this.matricula = matricula;
-//        this.descripcion = descripcion;
-//        this.propietario = propietario;
-//        this.dni_propietario = dni_propietario;
-//        this.num_kms = num_kms;
-//        this.precio = precio;
-//        this.fecha_matri = fecha_matri;
-//            
-                          
+                  v = new Vehiculo(marca, matricula, descripcion, propietario, dni, kilometros, precio, matri_fecha);
+                  //con el objeto vehiculo compelto, pasamos a añadir lo al array
+
+                  switch (concesionario.insertarVehiculo(v)) {
+                      case -2:
+                          System.out.println("El vehiculo existe");
+                          break;
+                      case -1:
+                          System.out.println("EL concesionario esta lleno");
+                          break;
+                      case 0:
+                          System.out.println("Vehiculo insertado correctamente");
+                          break;
+                  }
+
               break;
               case 2:
                   System.out.println("Listar Vehiculos");
@@ -189,19 +190,22 @@ public class Principal {
                   
               break;
               case 3:
+                  // buscar un vehiculo
                   System.out.println("Buscar Vehiculo");
                   System.out.println("Inserta matricula a buscar");
                   busca_matricula = teclado.next();
-                  
-                  // COMPROBAR ESTE CASO
-                  
-//                  String v = concesionario.buscaVehiculo(busca_matricula);
-                  
-                  if( v != null){
-                      System.out.println(v);
-                  }else{
-                      System.out.println("No existe vehiculo con la matricula introducida");
+
+                  // con buscaVehiculo buscamos el vehiculo y el string que devuelve lo metemos a la nueva variable
+                  String veh = concesionario.buscaVehiculo(busca_matricula);
+
+                  // ahora, si el string es null, porque no habia ningun coche y devolvio null
+                  //significa qu eno hay coche, si devuelve otra cosa, la imprimimos ya que es la info
+                  if (veh == null) {
+                      System.out.println("No existe Vehiculo con la matricula introducia");
+                  } else {
+                      System.out.println(veh);
                   }
+     
               break;
               case 4:
                   System.out.println("Modificar kilometros");
